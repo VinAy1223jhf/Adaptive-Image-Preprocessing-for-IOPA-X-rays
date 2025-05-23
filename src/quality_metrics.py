b047a3cc31b94a0049c08e3d7a198aada399cb95
Vinayak Garg <vinayakgarg91@gmail.com>
2025-05-23 12:45:40 +0530

    Update quality_metrics.py

--- src/quality_metrics.py
+++ src/quality_metrics.py
@@ -1,1 +1,16 @@
 
+def get_brightness(image):
+    return np.mean(image)
+
+def get_contrast(image):
+    return np.std(image)
+
+def get_sharpness(image):
+    image_uint8 = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    return cv2.Laplacian(image_uint8, cv2.CV_64F).var()
+
+def get_noise(image):
+    image_uint8 = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    blur = cv2.GaussianBlur(image_uint8, (3, 3), 0)
+    noise = image_uint8 - blur
+    return np.var(noise)
